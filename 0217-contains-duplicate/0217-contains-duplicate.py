54fca3f66ca9068b8d150fb18cad2be968304287
Aabha Mishra <72059223+aabhamishra@users.noreply.github.com>
2024-11-13 14:01:42 +0530

    Time: 15 ms (37.96%), Space: 25.1 MB (76.1%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        unique = set()
+        for num in nums:
+            if num in unique:
+                return True
+            unique.add(num)
+        
+        return False
+        
