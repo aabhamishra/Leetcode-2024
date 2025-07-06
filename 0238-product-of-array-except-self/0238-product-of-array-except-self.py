69f526dd99afc75f672f34ecf6f5be6627506ccc
Aabha Mishra <72059223+aabhamishra@users.noreply.github.com>
2025-07-06 14:40:00 +0530

    Time: 44 ms (29.68%), Space: 21.2 MB (36.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,26 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
 
-        # to calculate the product of an array except a given element at each position, we use prefix and suffix arrays.
-        # prefix array -> holds product of elements before that element
-        # suffix array -> holds product of elements after that element
-        # the result is the product of the prefix and siffux array
+        for i in range(1,len(nums)):
+            prefix[i] = prefix[i-1]*nums[i-1]
+        
+        for i in range(len(nums)-2, -1, -1):
+            suffix[i] = suffix[i+1]*nums[i+1]
 
-        # in this code, to say space, res holds the prefix array, and at the end is multiplied by the suffix array to get the right answer. 
-        nums_len = len(nums)
-        res = [1 for i in range(nums_len)]
-        suffix = [1 for i in range(nums_len)]
-
-        for i in range(1, nums_len):
-            res[i] = res[i-1] * nums[i-1]
+        for i in range(len(nums)):
+            prefix[i] *= suffix[i]
         
-        for i in range(nums_len - 2, -1, -1):
-            suffix[i] = suffix[i+1] * nums[i+1]
-            res[i] *= suffix[i]
-        
-        return res
-        
-        
+        return prefix
 
         
