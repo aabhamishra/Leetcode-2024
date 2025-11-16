e6595cd9ba07f3e0c49740bf3eca12b79d080906
Aabha Mishra <72059223+aabhamishra@users.noreply.github.com>
2025-11-16 12:14:06 +0530

    Time: 3 ms (90.29%), Space: 21.2 MB (80.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,37 +1,21 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        # initialize a frequency map: [number --> its frequency]
-        freq_map = {}
+from collections import Counter
+import heapq
 
-        # populate frequency map
-        for num in nums:
-            freq_map[num] = freq_map.get(num, 0) + 1
-        
-        bucket = [[] for i in range(len(nums) + 1)]
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        heap = []
 
-        for key,val in freq_map.items():
-            bucket[val].append(key)
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
 
-        res = []
-        res_len = 0
+            if len(heap) > k:
+                heapq.heappop(heap)
 
-        for i in range(len(bucket) - 1, -1, -1):
-            if bucket[i] != []:
-                for num in bucket[i]:
-                    res.append(num)
-                    res_len+=1
-            
-            if res_len >= k:
-                break
-        
-        return res[0:k]
-            
-        
+        result = []
+        for count, num in heap:
+            result.append(num)
 
-            
+        return result
+
         
